7ff9aac1558b91e60124d81aaf17459c1a838ba0
Jakub Strzelec <jakub_strzelec@o2.pl>
2024-04-11 22:31:40 +0200

    sync to repository

--- app/Http/Controllers/API/V1/ApiController.php
+++ app/Http/Controllers/API/V1/ApiController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\DB;
 class API extends Controller
 {
     public function GetRecipes() {
+        $list= DB::table('recipes')->get()->select(['title','preparation','ingredients','images_urls'])->toJson();
+        return $list;
+    }
+    private function searchForRecipe($word) {
         $list= DB::table('recipes')->get()->select(['title','preparation','ingredients','images_urls'])->toJson();
         return $list;
     }
