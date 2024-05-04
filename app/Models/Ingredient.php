67dbf58f33e97280d4afab7f0e3b4f170db36c66
Jakub Strzelec <jakub_strzelec@o2.pl>
2024-05-04 23:47:32 +0200

    -made new Model for Ingredients

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Ingredient extends Model
 {
     use HasFactory;
+    use Searchable;
+    protected $fillable = ['name','default_unit','avaiable_units','water','energy','protein','fat','carbohydrate','fiber','sugars','cink'];
 }
--- database/migrations/2024_04_21_134547_create_ingredients_table.php
+++ database/migrations/2024_04_21_134547_create_ingredients_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->string('unit');
+            $table->string('default_unit');
+            $table->json('avaiable_units');
             //nutriens
             $table->float('water');
             $table->float('energy');
